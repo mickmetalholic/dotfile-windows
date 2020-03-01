4108baf39ad3e231150a2a1b01ec0c21c04388d8
mickmetalholic <yuanshenmick@gmail.com>
2020-03-01 11:30:21 +0800

    update

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,13 +1,38 @@
 Import-Module posh-git
+
 Import-Module oh-my-posh
 Set-Theme Thunderfury
 
-If (-Not (Test-Path Variable:PSise)) {  # Only run this in the console and not in the ISE
-  Import-Module Get-ChildItemColor
-  
-  Set-Alias ll Get-ChildItem -option AllScope
-  Set-Alias ls Get-ChildItemColorFormatWide -option AllScope
+Import-Module PSColor
+$global:PSColor = @{
+    File = @{
+        Default    = @{ Color = 'White' }
+        Directory  = @{ Color = 'Cyan'}
+        Hidden     = @{ Color = 'DarkGray'; Pattern = '^\.' } 
+        Code       = @{ Color = 'Magenta'; Pattern = '\.(java|c|cpp|cs|js|css|html)$' }
+        Executable = @{ Color = 'Red'; Pattern = '\.(exe|bat|cmd|py|pl|ps1|psm1|vbs|rb|reg)$' }
+        Text       = @{ Color = 'Yellow'; Pattern = '\.(txt|cfg|conf|ini|csv|log|config|xml|yml|md|markdown)$' }
+        Compressed = @{ Color = 'Green'; Pattern = '\.(zip|tar|gz|rar|jar|war)$' }
+    }
+    Service = @{
+        Default = @{ Color = 'White' }
+        Running = @{ Color = 'DarkGreen' }
+        Stopped = @{ Color = 'DarkRed' }     
+    }
+    Match = @{
+        Default    = @{ Color = 'White' }
+        Path       = @{ Color = 'Cyan'}
+        LineNumber = @{ Color = 'Yellow' }
+        Line       = @{ Color = 'White' }
+    }
+	NoMatch = @{
+        Default    = @{ Color = 'White' }
+        Path       = @{ Color = 'Cyan'}
+        LineNumber = @{ Color = 'Yellow' }
+        Line       = @{ Color = 'White' }
+    }
 }
+
 
 function dotfiles {
   git --git-dir=$HOME/.dotfiles-windows.git --work-tree=$HOME @args
